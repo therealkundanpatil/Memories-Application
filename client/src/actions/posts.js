96593aa9fbbd9b7077d0713fff0939b353a7f740
therealkundanpatil <kp28021996@gmail.com>
2021-01-22 15:43:39 -0500

    -Introduced constants to avoid misspells

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,57 +1,58 @@
 import * as api from "../api";
+import { CREATE, UPDATE, DELETE, FETCH_ALL, LIKE } from "../constants/actionTypes";
 
 export const getPosts = () => async (dispatch) => {
-    try{
+    try {
         const { data } = await api.fetchPosts();
-        dispatch( { type: "FETCH_ALL", payload: data} );
+        dispatch({ type: FETCH_ALL, payload: data });
     }
-    catch(error){
+    catch (error) {
         console.log(error.message);
     }
-    
+
 }
 
 export const createPost = (post) => async (dispatch) => {
-    try{
-        const { data } = await api.createPost( post );
+    try {
+        const { data } = await api.createPost(post);
 
-        dispatch( { type:"CREATE", payload: data } );
+        dispatch({ type: CREATE, payload: data });
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
 
-export const updatePost = ( id, post ) => async (dispatch) => {
-    try{
-        const { data } = await api.updatePost( id, post );
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
 
-        dispatch( { type: "UPDATE", payload: data } );
+        dispatch({ type: UPDATE, payload: data });
 
     }
-    catch(error){
+    catch (error) {
         console.log(error)
     }
 }
 
-export const deletePost = (id) => async(dispatch) => {
-    try{
+export const deletePost = (id) => async (dispatch) => {
+    try {
         await api.deletePost(id);
 
-        dispatch( {type: "DELETE" , payload: id} );
+        dispatch({ type: DELETE, payload: id });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
     }
 }
 
 export const likePost = (id) => async (dispatch) => {
-    try{
-        const { data } = await api.likePost( id );
+    try {
+        const { data } = await api.likePost(id);
 
-        dispatch( { type: "LIKE", payload: data } );
+        dispatch({ type: LIKE, payload: data });
     }
-    catch( error ){
+    catch (error) {
         console.log(error);
     }
 }
