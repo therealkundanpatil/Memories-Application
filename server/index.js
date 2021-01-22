046113af16bd37a60333d8f99e3d38bb95c4fe11
therealkundanpatil <kp28021996@gmail.com>
2021-01-22 15:40:16 -0500

    -Changed Connection url and Port

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json( { limit: "30mb", extended: true } ));
@@ -15,10 +15,13 @@ app.use(bodyParser.urlencoded( { limit: "30mb", extended: true } ));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://therealkundanpatil:Kunpat@rt14@dypatil@cluster0.1kyvu.mongodb.net/<dbname>?retryWrites=true&w=majority";
+app.get("/", ( req, res ) => {
+    res.send("Hello there, Welcome to Memories API");
+});
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         .then( () => app.listen(PORT, () => console.log(`SERVER running on port: ${PORT}`)))
         .catch( (error) => console.log(error.message));
 
